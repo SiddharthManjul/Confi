98e9b3da4a9be62fa2b201b647bca2438b26dc51
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-10-10 23:55:35 +0530

    Delegator Account feature

---
+++ client/src/features/delegation.ts
@@ -0,0 +1,20 @@
+import { Implementation, toMetaMaskSmartAccount } from "@metamask/delegation-toolkit"
+import { createPublicClient, http } from "viem"
+import { sepolia as chain } from "viem/chains"
+import { privateKeyToAccount } from "viem/accounts"
+
+const publicClient = createPublicClient({
+  chain,
+  transport: http(),
+})
+
+const delegatorAccount = privateKeyToAccount("0x...")
+
+const delegatorSmartAccount = await toMetaMaskSmartAccount({
+  client: publicClient,
+  implementation: Implementation.Hybrid,
+  deployParams: [delegatorAccount.address, [], [], []],
+  deploySalt: "0x",
+  signer: { account: delegatorAccount },
+})
+
